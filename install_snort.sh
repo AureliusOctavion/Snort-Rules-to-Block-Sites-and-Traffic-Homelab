57db691c3a07ca8ef31968e17a8b35547d1e8d04
Siddharth Sriram <126032598+AureliusOctavion@users.noreply.github.com>
2025-03-07 14:07:05 -0600

    Update install_snort.sh

--- install_snort.sh
+++ install_snort.sh
@@ -1,3 +1,11 @@
 # install_snort.sh - Installs Snort and necessary directories
 sudo apt-get update
 sudo apt-get install -y snort
+
+# Create necessary directories for Snort
+sudo mkdir -p /usr/local/etc/rules
+sudo mkdir -p /usr/local/etc/so_rules/
+sudo mkdir -p /usr/local/etc/lists/
+sudo touch /usr/local/etc/rules/local.rules
+sudo touch /usr/local/etc/lists/default.blocklist
+sudo mkdir -p /var/log/snort
